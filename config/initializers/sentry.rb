cde67bbb6a13a3c7bf313a1edb560f26cfc09639
Radan Skorić <radan.skoric@gmail.com>
2024-08-19 15:02:47 +0200

    Fine tune settings
    Require master key to be set (it's already set on
    my fly.io deployment) and pull Sentry DSN from the
    configuration as well (dsn was rotated).

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,17 +1,7 @@
 Sentry.init do |config|
-  config.dsn = 'https://075a32220a48c4f8cb368760172fbb19@o4507670035628032.ingest.us.sentry.io/4507670035890176'
+  config.dsn = ENV["SENTRY_DSN"]
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
   config.traces_sample_rate = 0.1
-  # or
-  config.traces_sampler = lambda do |context|
-    true
-  end
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
   config.profiles_sample_rate = 0.2
 end
