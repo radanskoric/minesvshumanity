e274a0f2d4eba2d8ed4f55b83e2290aacb9efabd
Radan Skorić <radan.skoric@gmail.com>
2025-02-21 13:29:54 +0100

    Smaller screen size

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ Capybara.register_driver :selenium_headless do |app|
   options.add_argument("--headless")
   options.add_argument("--no-sandbox")
   options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--window-size=1400,900")
 
   Capybara::Selenium::Driver.new(
     app,
